733f382a23bc41097e7b29ee2efd64c0fae08673
metallicats <64232289+metallicats@users.noreply.github.com>
2020-08-17 12:55:29 +0100

    Update se-install-ubuntu.bash
    Added squid install

--- ubuntu/se-install-ubuntu.bash
+++ ubuntu/se-install-ubuntu.bash
@@ -94,7 +94,7 @@ do
         break
             ;;
         "No")
-        wget -O vpnserver-init https://raw.githubusercontent.com/icoexist/softether-autoinstall/master/vpnserver-init > /dev/null 2>&1
+        wget -O vpnserver-init https://raw.githubusercontent.com/metallicats/softether-autoinstall/master/vpnserver-init > /dev/null 2>&1
         mv vpnserver-init /etc/init.d/vpnserver
         chmod 755 /etc/init.d/vpnserver
         printf "\nSystem daemon created. Registering changes...\n\n"
@@ -105,6 +105,12 @@ do
         cd && rm -rf /tmp/softether-autoinstall > /dev/null 2>&1
         systemctl is-active --quiet vpnserver && echo "Service vpnserver is running."
         printf "\n${RED}!!! IMPORTANT !!!${NC}\n\nTo configure the server, use the SoftEther VPN Server Manager located here: http://bit.ly/2D30Wj8 or use ${RED}sudo /opt/vpnserver/vpncmd${NC}\n\n${RED}!!! UFW is not enabled with this script !!!${NC}\n\nTo see how to open ports for SoftEther VPN, please go here: http://bit.ly/2JdZPx6\n\nNeed help? Feel free to join the Discord server: https://icoexist.io/discord\n\n"
+        printf "\nPress anything to Install Squid.\n\n"
+        apt install squid -y
+        Proxy_Port='7835'
+        echo -e "acl VPN dst $(wget -4qO- http://ipinfo.io/ip)/32\nhttp_access allow VPN\nhttp_access deny all\nhttp_port 0.0.0.0:$Proxy_Port\nacl all src 0.0.0.0/0.0.0.0\nno_cache deny all\ndns_nameservers 1.1.1.1 1.0.0.1\nvisible_hostname localhost" > /etc/squid/squid.conf
+        service squid restart
+        printf "\nSquid successfully installed.\n\n"
         break
             ;;
         "Quit")
